904e58b791dc465cb40ec12f9b53e350dca87337
Axel <axelviskovic@gmail.com>
2019-12-15 13:14:38 +0100

    apparition farm
    done

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,10 @@ const buttonStart = document.querySelector('.buttonStart')
 const elementsDecorSavane = document.querySelectorAll('.backgroundSavaneContainer>img') // TOUTES LES IMAGES DE SAVANE
 const musicSavane = document.querySelector('#musicSavane')
 const fusilSound = document.querySelector('#fusilSound')
+const fusilEmptySound= document.querySelector('#fusilEmptySound')
+const elementsDecorFerme = document.querySelectorAll('.backgroundFermeContainer>img') // TOUTES LES IMAGES DE SAVANE
+const backgroundFermeContainer=document.querySelector('.backgroundFermeContainer')
+const backgroundSavaneContainer=document.querySelector('.backgroundSavaneContainer')
 
 buttonStart.addEventListener(
   'click',
@@ -22,13 +26,28 @@ buttonStart.addEventListener(
       lionContainer.style.display='block'
       lionContainer.classList.add('animationMoving')
     }
+    let changeBg=setTimeout(changeBackground,20000)
+    function changeBackground(){
+      for(let i=0; i<elementsDecorSavane.length; i++){
+        elementsDecorSavane[i].classList.add('animationDepop')
+        let apparitionFerme = setTimeout(ferme,3000)
+        function ferme(){
+          backgroundSavaneContainer.style.display='none'
+          for(let i=0; i<elementsDecorFerme.length; i++){
+            backgroundFermeContainer.style.display='block'
+            elementsDecorFerme[i].classList.add('animationPop')
+          }
+        }
+      }
+    }
+
   }
 )
 
-
-// ANIMATION ANIMAUX
-
-
+function emptyGun(){
+  fusilEmptySound.play()
+  fusilEmptySound.currentTime=0
+}
 
 // BALLES
 
@@ -46,22 +65,27 @@ let nbMunitions = 3,
     currentAnimal = ''
 
     function shoot(){
-      fusilSound.play()
-      fusilSound.currentTime=0
-      if(nbMunitions==1){
-        balle3.style.display='none'
-        balleEmpty3.style.display='block'
-        nbMunitions--
+      if(nbMunitions>0){
+        fusilSound.play()
+        fusilSound.currentTime=0
+        if(nbMunitions==1){
+          balle3.style.display='none'
+          balleEmpty3.style.display='block'
+          nbMunitions--
+        }
+        if(nbMunitions==2){
+          balle2.style.display='none'
+          balleEmpty2.style.display='block'
+          nbMunitions--
+        }
+        if(nbMunitions==3){
+          balle1.style.display='none'
+          balleEmpty1.style.display='block'
+          nbMunitions--
+        }
       }
-      if(nbMunitions==2){
-        balle2.style.display='none'
-        balleEmpty2.style.display='block'
-        nbMunitions--
-      }
-      if(nbMunitions==3){
-        balle1.style.display='none'
-        balleEmpty1.style.display='block'
-        nbMunitions--
+      else{
+        emptyGun()
       }
     }
 
@@ -77,15 +101,20 @@ const lionMort = document.querySelector('#lionMort')
 elephant.addEventListener(
   'click',
   ()=>{
-    shoot()
-    elephantContainer.style.left=elephantContainer.offsetLeft+'px'
-    elephant.style.display='none'
-    elephantMort.style.display='block'
-    elephantContainer.classList.remove('animationMoving')
-    let disparition=setTimeout(opacityDecrease,2000)
-    function opacityDecrease(){
-      elephantContainer.style.transition='2s'
-      elephantContainer.style.opacity='0'
+    if(nbMunitions>0){
+      shoot()
+      elephantContainer.style.left=elephantContainer.offsetLeft+'px'
+      elephant.style.display='none'
+      elephantMort.style.display='block'
+      elephantContainer.classList.remove('animationMoving')
+      let disparition=setTimeout(opacityDecrease,2000)
+      function opacityDecrease(){
+        elephantContainer.style.transition='2s'
+        elephantContainer.style.opacity='0'
+      }
+    }
+    else{
+      emptyGun()
     }
   }
 )
@@ -93,15 +122,20 @@ elephant.addEventListener(
 lion.addEventListener(
   'click',
   ()=>{
-    shoot()
-    lionContainer.style.left=lionContainer.offsetLeft+'px'
-    lion.style.display='none'
-    lionMort.style.display='block'
-    lionContainer.classList.remove('animationMoving')
-    let disparition=setTimeout(opacityDecrease,2000)
-    function opacityDecrease(){
-      lionContainer.style.transition='2s'
-      lionContainer.style.opacity='0'
+    if(nbMunitions>0){
+      shoot()
+      lionContainer.style.left=lionContainer.offsetLeft+'px'
+      lion.style.display='none'
+      lionMort.style.display='block'
+      lionContainer.classList.remove('animationMoving')
+      let disparition=setTimeout(opacityDecrease,2000)
+      function opacityDecrease(){
+        lionContainer.style.transition='2s'
+        lionContainer.style.opacity='0'
+      }
+    }
+    else{
+      emptyGun()
     }
   }
 )
@@ -116,3 +150,11 @@ lion.addEventListener(
 
     }
   )
+
+
+
+
+
+
+
+//a
